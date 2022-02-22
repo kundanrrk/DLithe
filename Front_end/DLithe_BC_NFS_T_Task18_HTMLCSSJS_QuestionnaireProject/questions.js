ae995a81fc67046a35b0079afc08fd5cfed55596
Kundan <kundan.rrk21@gmail.com>
2022-02-23 01:39:01 +0530

    third commit

---
+++ Front_end/DLithe_BC_NFS_T_Task18_HTMLCSSJS_QuestionnaireProject/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What is part of a database that holds only one type of information?",
+        answer: "Field",
+        options: [
+            "Report",
+            "Field",
+            "Record",
+            "File"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The most common format for a home video recorder is VHS. VHS stands for...?",
+        answer: "Video Home System",
+        options: [
+            "Very high speed",
+            "Video Home System",
+            "Voltage house standard",
+            "Video horizontal standard"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does the term PLC stand for ?",
+        answer: "Programmable Logic Controller",
+        options: [
+            "Programmable Lift Computer",
+            "Program List Control",
+            "Programmable Logic Controller",
+            "Piezo Lamp Connector"
+        ]
+    },
+    {
+        numb: 4,
+        question: "'.INI' extension refers usually to what kind of file?",
+        answer: "System file",
+        options: [
+            "Image file",
+            "Hypertext related file",
+            "Image Color Matching Profile file",
+            "System file"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What will a UPS be used for in a building?",
+        answer: "To provide power to essential equipment",
+        options: [
+            "To provide power to essential equipment",
+            "To monitor building electricity use",
+            "To carry messages between departments",
+            "To control lighting and power systems"
+        ]
+    },
+
+];
