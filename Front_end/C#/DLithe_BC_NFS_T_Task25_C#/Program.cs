3d33afe3153665ce839e844b59e0124c0767d553
Kundan <kundan.rrk21@gmail.com>
2022-02-25 11:10:33 +0530

    commit

---
+++ Front_end/C#/DLithe_BC_NFS_T_Task25_C#/Program.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+
+namespace EncapInher
+{
+
+
+    class Students
+    {
+
+        private String studentName;
+        private int studentAge;
+
+
+        public String Name
+        {
+
+            get
+            {
+                return studentName;
+            }
+
+            set
+            {
+                studentName = value;
+            }
+
+        }
+
+
+        public int Age
+        {
+
+            get
+            {
+                return studentAge;
+            }
+
+            set
+            {
+                studentAge = value;
+            }
+
+        }
+    }
+
+    class MainClass { 
+   public  static void Main(string[] args)
+    {
+        Student student = new Student();
+        Student student1 = new Student();
+        Students private1 = new Students();
+        Students private2 = new Students();
+
+
+
+        student.ID = "101";
+        student.Name = "Kundan Kumar";
+        student.Email = "Kundan@rrk21@.com";
+        student.phone_no = 1234567890;
+        student.dept = "IT";
+
+
+        student1.ID = "103";
+        student1.Name = "Nayan Kumar";
+        student1.Email = "Nayan@rrk21@.com";
+        student1.phone_no = 1234554547;
+        student1.dept = "IT";
+
+
+
+        Console.WriteLine("ID = " + student.ID);
+        Console.WriteLine("Name = " + student.Name);
+        Console.WriteLine("Email = " + student.Email);
+        Console.WriteLine("Contact no = " + student.phone_no);
+        Console.WriteLine("Department = " + student.dept);
+
+        Console.WriteLine("ID = " + student1.ID);
+        Console.WriteLine("Name = " + student1.Name);
+        Console.WriteLine("Email = " + student.Email);
+        Console.WriteLine("Contact no = " + student.phone_no);
+        Console.WriteLine("Department = " + student.dept);
+
+        private1.Name = "Kundan Kumar";
+        private1.Age = 25;
+
+        private2.Name = "Mahesh Kumar";
+        private2.Age = 27;
+
+
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("----accesing Private----");
+
+
+        Console.WriteLine("Name:-  " + private1.Name);
+        Console.WriteLine("Name:-  " + private1.Age);
+
+        Console.WriteLine("Name:-  " + private2.Name);
+        Console.WriteLine("Name:-  " + private2.Age);
+
+
+    }
+}
+}
+
---
+++ Front_end/C#/DLithe_BC_NFS_T_Task25_C#/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace EncapInher
+{
+    class Student
+    {
+
+        // Creating setter and getter for each property  
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int phone_no { get; set; }
+
+        public string dept { get; set; }
+
+    }
+}
